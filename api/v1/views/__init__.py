aca0d18f877bf5215f92448a24337bc2e4db5f6f
Goldenlady0940 <sebrinaa18@gmail.com>
2023-01-23 22:18:27 +0300

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ app_views = Blueprint('app_view', __name__, url_prefix='/api/v1/')
 from api.v1.views.index import *
 from api.v1.views import states, cities, amenities, users
 from api.v1.views import places, places_reviews
+from api.v1.views import places_amenities
